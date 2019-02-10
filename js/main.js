9e8b73ed75226b2230bc0f9989c9ba4c425472f0
mischoko <michal.zak.misch@gmail.com>
2019-02-10 20:32:54 +0100

    edit on menu

--- js/main.js
+++ js/main.js
@@ -5,12 +5,14 @@ window.onscroll = function() {
     var color1 = document.getElementsByClassName('menuItem')[1];
     var color2 = document.getElementsByClassName('menuItem')[2];
     var color3 = document.getElementsByClassName('menuItem')[3];
+    var fix = document.getElementById('menu');
     if ( window.pageYOffset > 100 ) {
             nav.classList.add("blackNav");
             color0.classList.add("whiteOut");
             color1.classList.add("whiteOut");
             color2.classList.add("whiteOut");
             color3.classList.add("whiteOut");
+            fix.classList.add("menuFix");
     }
     else {
             nav.classList.remove("blackNav");
@@ -18,5 +20,6 @@ window.onscroll = function() {
             color1.classList.remove("whiteOut");
             color2.classList.remove("whiteOut");
             color3.classList.remove("whiteOut");
+            fix.classList.remove("menuFix");
     }
 }
