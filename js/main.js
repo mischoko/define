4c1d3d5094bea6b33b4dde4a52b954b82400669f
mischoko <michal.zak.misch@gmail.com>
2019-02-10 18:25:51 +0100

    separated js added menu anim

---
+++ js/main.js
@@ -0,0 +1,22 @@
+    //menu functionality
+window.onscroll = function() {
+    var nav = document.getElementById('nav');
+    var color0 = document.getElementsByClassName('menuItem')[0];
+    var color1 = document.getElementsByClassName('menuItem')[1];
+    var color2 = document.getElementsByClassName('menuItem')[2];
+    var color3 = document.getElementsByClassName('menuItem')[3];
+    if ( window.pageYOffset > 100 ) {
+            nav.classList.add("blackNav");
+            color0.classList.add("whiteOut");
+            color1.classList.add("whiteOut");
+            color2.classList.add("whiteOut");
+            color3.classList.add("whiteOut");
+    }
+    else {
+            nav.classList.remove("blackNav");
+            color0.classList.remove("whiteOut");
+            color1.classList.remove("whiteOut");
+            color2.classList.remove("whiteOut");
+            color3.classList.remove("whiteOut");
+    }
+}
